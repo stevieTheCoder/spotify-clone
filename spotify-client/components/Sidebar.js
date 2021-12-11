6b9c04f885fd29573ec9124b4cfe38c4fa59d02d
Steven Rogers <steven.rogers1990@gmail.com>
2021-12-11 20:56:19 +0000

    Sidebar component basics

---
+++ spotify-client/components/Sidebar.js
@@ -0,0 +1,48 @@
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return (
+    <div className="p-5 text-sm text-gray-500 border-r border-gray-900">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="w-5 h-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="w-5 h-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="w-5 h-5" />
+          <p>Your Library</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="w-5 h-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="w-5 h-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="w-5 h-5" />
+          <p>Your Eposiodes</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+
+        {/* Playlists will be rendered here */}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
