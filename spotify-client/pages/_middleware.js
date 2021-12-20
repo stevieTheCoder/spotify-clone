0b0e11e9fd65b930d637a616c04fab2b4619fd24
Steven Rogers <steven.rogers1990@gmail.com>
2021-12-20 20:55:33 +0000

    Re implement middleware

--- spotify-client/pages/_middleware.js
+++ spotify-client/pages/_middleware.js
@@ -3,26 +3,24 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   // Token will exist if user is logged in
-
-  console.log("Here is the JWT secret", process.env.JWT_SECRET);
+  console.log("request", req);
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  console.log("token", token);
 
-  console.log(">> Here is the token", token);
+  const { pathname } = req.nextUrl;
 
-  // const { pathname } = req.nextUrl;
+  // Redirect to home page if accessing login when already authenticated
+  if (pathname.includes("/login") && token) {
+    return NextResponse.redirect("/");
+  }
 
-  // // Redirect to home page if accessing login when already authenticated
-  // if (pathname.includes("/login") && token) {
-  //   return NextResponse.redirect("/");
-  // }
+  // Allow request if request for session & provider is fetching or if token exists
+  if (pathname.includes("/api/auth" || token)) {
+    return NextResponse.next();
+  }
 
-  // // Allow request if request for session & provider is fetching or if token exists
-  // if (pathname.includes("/api/auth" || token)) {
-  //   return NextResponse.next();
-  // }
-
-  // // Redirect to login if user is trying to access protected route and has not been authenticated
-  // if (!token && pathname !== "/login") {
-  //   return NextResponse.redirect("/login");
-  // }
+  // Redirect to login if user is trying to access protected route and has not been authenticated
+  if (!token && pathname !== "/login") {
+    return NextResponse.redirect("/login");
+  }
 }
