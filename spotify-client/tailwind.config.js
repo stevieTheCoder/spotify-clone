b416d478d7bd4903ad791abaac443f06cb9cca1d
Steven Rogers <steven.rogers1990@gmail.com>
2021-12-21 13:27:54 +0000

    Fix for mobile height

--- spotify-client/tailwind.config.js
+++ spotify-client/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    height: (theme) => ({
+      auto: "auto",
+      ...theme("spacing"),
+      full: "100%",
+      screen: "calc(var(--vh) * 100)",
+    }),
+    minHeight: (theme) => ({
+      0: "0",
+      ...theme("spacing"),
+      full: "100%",
+      screen: "calc(var(--vh) * 100)",
+    }),
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
