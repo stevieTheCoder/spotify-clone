337674a4d2490b64a64246358e1dc7d48659e8fe
Steven Rogers <steven.rogers1990@gmail.com>
2021-12-31 11:14:30 +0000

    Moving to static spotify image

--- spotify-client/src/pages/login.tsx
+++ spotify-client/src/pages/login.tsx
@@ -1,53 +1,22 @@
-import { GetServerSideProps } from "next";
-import { BuiltInProviderType } from "next-auth/providers";
-import { ClientSafeProvider, getProviders, LiteralUnion, signIn } from "next-auth/react";
-import PropTypes from "prop-types";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import spotifyLogo from "/public/spotify.png";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const providers = await getProviders();
-  const { callbackUrl } = context.query;
-
-  return {
-    props: {
-      providers,
-      callbackUrl: callbackUrl ?? "/",
-    },
-  };
-}
-
-interface Props {
-  providers: Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null
-  callbackUrl: string
-}
-
-export const Login: React.FC<Props> = ({ providers, callbackUrl }) => {
- 
+export const Login: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen bg-black">
-      <img
-        className="mb-5 w-52"
-        src="https://links.papareact.com/9xl"
-        alt="spotify logo"
-      />
+      <div className="mb-5">
+        <Image src={spotifyLogo} alt="spotify logo"></Image>
+      </div>
 
-      
-      {providers == null ? null : Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button
-            className="bg-[#18D860] text-white p-5 rounded-full"
-            onClick={() => signIn(provider.id, { callbackUrl })}
-          >
-            Login with {provider.name}
-          </button>
-        </div>
-      ))}
+      <button
+        className="w-52 bg-[#18D860] text-white p-5 rounded-full"
+        onClick={() => signIn("spotify", { callbackUrl: "/" })}
+      >
+        Login with Spotify
+      </button>
     </div>
   );
-}
-
-Login.propTypes = {
-  providers: PropTypes.any,
-  callbackUrl: PropTypes.string.isRequired,
 };
 
 export default Login;
